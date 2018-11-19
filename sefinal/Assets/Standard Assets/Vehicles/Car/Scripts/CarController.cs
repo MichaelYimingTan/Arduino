e258932275b4c7893af729bdc9c5dec838b8ce47
hkeung <hkeung@uwaterloo.ca>
2018-11-18 19:21:31 -0500

    cleaned up code

--- sefinal/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ sefinal/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -9,13 +9,6 @@ namespace UnityStandardAssets.Vehicles.Car
         RearWheelDrive,
         FourWheelDrive
     }
-
- //   internal enum SpeedType
-   // {
-     //   MPH,
-       // KPH
-//    }
-
     public class CarController : MonoBehaviour
     {
         [SerializeField] private CarDriveType m_CarDriveType = CarDriveType.FourWheelDrive;
@@ -28,10 +21,10 @@ namespace UnityStandardAssets.Vehicles.Car
         [Range(0, 1)] [SerializeField] private float m_TractionControl; // 0 is no traction control, 1 is full interference
         [SerializeField] private float m_FullTorqueOverAllWheels;
         [SerializeField] private float m_ReverseTorque;
-        [SerializeField] private float m_MaxHandbrakeTorque;
+      //  [SerializeField] private float m_MaxHandbrakeTorque;
         [SerializeField] private float m_Downforce = 100f;
     //    [SerializeField] private SpeedType m_SpeedType;
-        [SerializeField] private float m_Topspeed = 200;
+        [SerializeField] private float m_Topspeed = 20000;
         [SerializeField] private static int NoOfGears = 5;
         [SerializeField] private float m_RevRangeBoundary = 1f;
         [SerializeField] private float m_SlipLimit;
@@ -51,7 +44,7 @@ namespace UnityStandardAssets.Vehicles.Car
         public float BrakeInput { get; private set; }
         public float CurrentSteerAngle{ get { return m_SteerAngle; }}
         public float CurrentSpeed{ get { return m_Rigidbody.velocity.magnitude*2.23693629f; }}
-        public float MaxSpeed{get { return m_Topspeed; }}
+     //   public float MaxSpeed{get { return m_Topspeed; }}
         public float Revs { get; private set; }
         public float AccelInput { get; private set; }
 
@@ -63,51 +56,16 @@ namespace UnityStandardAssets.Vehicles.Car
             {
                 m_WheelMeshLocalRotations[i] = m_WheelMeshes[i].transform.localRotation;
             }
-            m_WheelColliders[0].attachedRigidbody.centerOfMass = m_CentreOfMassOffset;
+           // m_WheelColliders[0].attachedRigidbody.centerOfMass = m_CentreOfMassOffset;
 
-            m_MaxHandbrakeTorque = float.MaxValue;
+           // m_MaxHandbrakeTorque = float.MaxValue;
 
             m_Rigidbody = GetComponent<Rigidbody>();
             m_CurrentTorque = m_FullTorqueOverAllWheels - (m_TractionControl*m_FullTorqueOverAllWheels);
         }
 
 
-        // simple function to add a curved bias towards 1 for a value in the 0-1 range
-        private static float CurveFactor(float factor)
-        {
-            return 1 - (1 - factor)*(1 - factor);
-        }
-
-
-        // unclamped version of Lerp, to allow value to exceed the from-to range
-        private static float ULerp(float from, float to, float value)
-        {
-            return (1.0f - value)*from + value*to;
-        }
-
-
-   /*     private void CalculateGearFactor()
-        {
-            float f = (1/(float) NoOfGears);
-            // gear factor is a normalised representation of the current speed within the current gear's range of speeds.
-            // We smooth towards the 'target' gear factor, so that revs don't instantly snap up or down when changing gear.
-            var targetGearFactor = Mathf.InverseLerp(f*m_GearNum, f*(m_GearNum + 1), Mathf.Abs(CurrentSpeed/MaxSpeed));
-            m_GearFactor = Mathf.Lerp(m_GearFactor, targetGearFactor, Time.deltaTime*5f);
-        }
-
-
-        private void CalculateRevs()
-        {
-            // calculate engine revs (for display / sound)
-            // (this is done in retrospect - revs are not used in force/power calculations)
-            CalculateGearFactor();
-            var gearNumFactor = m_GearNum/(float) NoOfGears;
-            var revsRangeMin = ULerp(0f, m_RevRangeBoundary, CurveFactor(gearNumFactor));
-            var revsRangeMax = ULerp(m_RevRangeBoundary, 1f, gearNumFactor);
-            Revs = ULerp(revsRangeMin, revsRangeMax, m_GearFactor);
-        }*/
-
-
+      
         public void Move(float steering, float accel, float footbrake, float handbrake)
         {
             for (int i = 0; i < 4; i++)
@@ -133,46 +91,14 @@ namespace UnityStandardAssets.Vehicles.Car
 
             SteerHelper();
             ApplyDrive(accel, footbrake);
-            CapSpeed();
-
-            //Set the handbrake.
-            //Assuming that wheels 2 and 3 are the rear wheels.
-            if (handbrake > 0f)
-            {
-                var hbTorque = handbrake*m_MaxHandbrakeTorque;
-                m_WheelColliders[2].brakeTorque = hbTorque;
-                m_WheelColliders[3].brakeTorque = hbTorque;
-            }
+        
 
 
-         //   CalculateRevs();
-
-            AddDownForce();
-            CheckForWheelSpin();
             TractionControl();
         }
 
 
-        private void CapSpeed()
-        {
-            float speed = m_Rigidbody.velocity.magnitude;
-          //  switch (m_SpeedType)
-            //{
-              //  case SpeedType.MPH:
-
-                    speed *= 2.23693629f;
-                    if (speed > m_Topspeed)
-                        m_Rigidbody.velocity = (m_Topspeed/2.23693629f) * m_Rigidbody.velocity.normalized;
-                //    break;
-
-               // case SpeedType.KPH:
-                 //   speed *= 3.6f;
-                   // if (speed > m_Topspeed)
-                     //   m_Rigidbody.velocity = (m_Topspeed/3.6f) * m_Rigidbody.velocity.normalized;
-                   // break;
-      //      }
-        }
-
+ 
 
         private void ApplyDrive(float accel, float footbrake)
         {
@@ -235,51 +161,7 @@ namespace UnityStandardAssets.Vehicles.Car
             m_OldRotation = transform.eulerAngles.y;
         }
 
-
-        // this is used to add more grip in relation to speed
-        private void AddDownForce()
-        {
-            m_WheelColliders[0].attachedRigidbody.AddForce(-transform.up*m_Downforce*
-                                                         m_WheelColliders[0].attachedRigidbody.velocity.magnitude);
-        }
-
-
-        // checks if the wheels are spinning and is so does three things
-        // 1) emits particles
-        // 2) plays tiure skidding sounds
-        // 3) leaves skidmarks on the ground
-        // these effects are controlled through the WheelEffects class
-        private void CheckForWheelSpin()
-        {
-            // loop through all wheels
-            for (int i = 0; i < 4; i++)
-            {
-                WheelHit wheelHit;
-                m_WheelColliders[i].GetGroundHit(out wheelHit);
-
-                // is the tire slipping above the given threshhold
-                if (Mathf.Abs(wheelHit.forwardSlip) >= m_SlipLimit || Mathf.Abs(wheelHit.sidewaysSlip) >= m_SlipLimit)
-                {
-       //            m_WheelEffects[i].EmitTyreSmoke();
-
-                    // avoiding all four tires screeching at the same time
-                    // if they do it can lead to some strange audio artefacts
-            /*       if (!AnySkidSoundPlaying())
-                    {
-         //               m_WheelEffects[i].PlayAudio();
-                    }*/
-                    continue;
-                }
-
-                // if it wasnt slipping stop all the audio
-         //       if (m_WheelEffects[i].PlayingAudio)
-           //     {
-             //       m_WheelEffects[i].StopAudio();
-               // }
-                // end the trail generation
-                //m_WheelEffects[i].EndSkidTrail();
-            }
-        }
+   
 
         // crude traction control that reduces the power to wheel if the car is wheel spinning too much
         private void TractionControl()
@@ -315,7 +197,7 @@ namespace UnityStandardAssets.Vehicles.Car
             }
         }
 
-
+        
         private void AdjustTorque(float forwardSlip)
         {
             if (forwardSlip >= m_SlipLimit && m_CurrentTorque >= 0)
@@ -331,18 +213,7 @@ namespace UnityStandardAssets.Vehicles.Car
                 }
             }
         }
+        
 
-
-    /*   private bool AnySkidSoundPlaying()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (m_WheelEffects[i].PlayingAudio)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }*/
     }
 }
