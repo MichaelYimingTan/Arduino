b432ef56eedfb6ae0df3abc1fa1fc51106af9779
Yiming Tan <y62tan@uwaterloo.ca>
2018-11-22 11:15:55 +0800

    adding background music

---
+++ sefinal/Assets/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake() {
+        foreach (Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+
+            s.source.loop = s.looping; 
+        }
+	}
+
+    void Start()
+    {
+        Playing("Background");
+    }
+
+    public void Playing (string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null){
+            return;
+        }
+        s.source.Play();
+    }
+}
